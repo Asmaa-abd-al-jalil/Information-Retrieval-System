7a421dc8e0714ea75e95c2ed41b79c04c7fb81b8
OlaMorad <ola.morad2003@gmail.com>
2026-06-25 14:11:16 +0300

    CU-86baje0hu fix the structure

--- services/data_service.py
+++ services/data_service.py
@@ -1,20 +1,21 @@
 import os
-import ir_datasets
-from ir_datasets.util import fileio
-from services.preprocessing_service import preprocess_text
 
 os.environ["PYTHONUTF8"] = "1"
 os.environ["IR_DATASETS_HOME"] = "D:\\ir_storage"
 os.environ["IR_DATASETS_TMP"] = "D:\\ir_tmp"
-
 os.makedirs("D:\\ir_storage", exist_ok=True)
 os.makedirs("D:\\ir_tmp", exist_ok=True)
 
+import ir_datasets
+from ir_datasets.util import fileio
+from services.preprocessing_service import preprocess_text
+
 fileio.verify = lambda: None
 
-DATASET_ID = "beir/nq"
+DATASET_ID = "clinicaltrials/2019/trec-pm-2019"
+
 SUPPORTED_DATASETS = {
-    "beir-nq": DATASET_ID
+    "clinicaltrials_pm": DATASET_ID
 }
 
 def get_dataset(ds_id: str = DATASET_ID):
